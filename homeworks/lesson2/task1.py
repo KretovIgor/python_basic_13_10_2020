189276b289c42d6012ddd611dbc4c54a74c8fee7
KretovIgor <kretovigor@bk.ru>
2020-10-21 19:26:10 +0300

    done task2.py les2, fix task1.py

--- homeworks/lesson2/task1.py
+++ homeworks/lesson2/task1.py
@@ -1,4 +1,3 @@
-my_string = ['абракадабра', (1,2,3,4), [4,3,2,1], {'power': None}, 1, 1.0]
+my_string = ['абракадабра', (1, 2, 3, 4), [4, 3, 2, 1], {'power': None}, 1, 1.0]
 for item in my_string:
-
     print(type(item))
