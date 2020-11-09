6de245cdecd9605524d0bb091f319b06af84dc6d
KretovIgor <kretovigor@bk.ru>
2020-11-09 19:26:45 +0300

    done task2.py les3

--- homeworks/lesson3/task2.py
+++ homeworks/lesson3/task2.py
@@ -8,24 +8,17 @@
 
 
 def user():
-    global name, surname, birth_year, city, email, phone
+    try:
+        name = input('Введите имя ')
+        surname = input('Введите фамилию ')
+        birth_year = int(input('Введите год рождения '))
+        city = input('Введите город проживания ')
+        email = input('Введите свой email ')
+        phone = int(input('Введите номер телефона (10 цифр) '))
+    except (NameError, ValueError):
+        return 'Введено неверное значение'
     user_data = f'{surname} {name} родился в {birth_year} году, в городе {city}, эл.почта {email}, телефон {phone}'
     return user_data
 
 
-try:
-    name = input('Введите имя ')
-    surname = input('Введите фамилию ')
-    birth_year = int(input('Введите год рождения '))
-    city = input('Введите город проживания ')
-    email = input('Введите свой email ')
-    phone = int(input('Введите номер телефона (10 цифр) '))
-except (NameError, ValueError):
-    print('Введено неверное значение')
-
 print(user())
-
-# print(f'{surname} {name} родился в {birth_year} году, в городе {city}, эл.почта {email}, телефон {phone}')
-
-
-# user(name=get_name, surname=get_surname, birth_year=get_birth_year, city=get_city, email=get_email, phone=get_phone)
